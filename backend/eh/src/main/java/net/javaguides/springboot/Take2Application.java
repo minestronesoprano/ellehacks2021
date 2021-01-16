66404c2a3f186436adb526d00bf8989553880b07
Michelle Berry <63769834+michelleberry@users.noreply.github.com>
2021-01-16 11:09:21 -0500

    added expenses/income

--- backend/eh/src/main/java/net/javaguides/springboot/Take2Application.java
+++ backend/eh/src/main/java/net/javaguides/springboot/Take2Application.java
@@ -28,7 +28,8 @@ public class Take2Application implements CommandLineRunner {
 	  this.userRepository.save(new User("Michelle", "Berry", "2"));
 	  this.userRepository.save(new User("Maya", "Shankar", "3"));
 	  this.userRepository.save(new User("Angeli", "Manipon", "4"));
-	  this.scenarioRepository.save(new Scenario("Your friends want you to buy them ice cream. It will cost you 10$ at McDonald's.", "Yes, buy the icecream", -10, 0, "No, save my money", 0, 0)); 
+	  this.scenarioRepository.save(new Scenario("Your friends want you to buy them ice cream. It will cost you 10$ at McDonald's.", "Yes, buy the icecream", -10, 0, 0, 0, "No, save my money", 0, 0, 0, 0)); 
+	  this.scenarioRepository.save(new Scenario("You are offered a job at the local McDonald's. It will cost you $15 for the uniform, but you will make about $320 dollars a week working part-time.", "Yes, take the job offer", -15, 0, 320, 0, "No thank you", 0, 0, 0, 0));
 	}
 
 }
--- backend/eh/src/main/java/net/javaguides/springboot/model/Scenario.java
+++ backend/eh/src/main/java/net/javaguides/springboot/model/Scenario.java
@@ -27,6 +27,14 @@ public class Scenario {
   @Column(name = "option1debt")
   private int option1debt;
 
+  //indicates an addition to the person's regular month income
+  @Column(name = "option1income")
+  private int option1income; 
+
+  //indicates a increase of the person's regular month expenses
+  @Column(name = "option1expenses")
+  private int option1expenses;
+
   @Column(name = "option2")
   private String option2; 
   
@@ -36,13 +44,44 @@ public class Scenario {
   @Column(name = "option2debt")
   private int option2debt;
 
-  public Scenario( String description, String option1, int option1savings, int option1debt, String option2, int option2savings, int option2debt){
+  //indicates an addition to the person's regular month income
+  @Column(name = "option2income")
+  private int option2income; 
+
+  //indicates a increase of the person's regular month expenses
+  @Column(name = "option2expenses")
+  private int option2expenses;
+
+  public Scenario(){
+    this.description = "description"; 
+
+    this.option1 = "option1"; 
+    this.option1savings = 0; 
+    this.option1debt = 0; 
+    this.option1income = 0; 
+    this.option1expenses = 0; 
+
+    this.option2 = "option2"; 
+    this.option2savings = 0; 
+    this.option2debt = 0; 
+    this.option2income = 0; 
+    this.option2expenses = 0;
+  }
+
+  public Scenario( String description, String option1, int option1savings, int option1debt, int option1income, 
+  int option1expense, String option2, int option2savings, int option2debt, int option2income, int option2expenses){
     this.description = description; 
+
     this.option1 = option1; 
     this.option1savings = option1savings; 
     this.option1debt = option1debt; 
+    this.option1income = option1income; 
+    this.option1expense = option1expense; 
+
     this.option2 = option2; 
     this.option2savings = option2savings; 
     this.option2debt = option2debt; 
+    this.option2income = option2income; 
+    this.option2expense = option2expense;
   }
 }
